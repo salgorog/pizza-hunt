4cc9001af688b246cdfb8a6e1b670a0aaf0a4808
Aztec <salgorog@hotmail.com>
2020-12-07 21:15:04 -0700

    finished the comment and controller routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -38,6 +38,17 @@ const commentController = {
             .catch(err => res.json(err));
     },
 
+    // remove reply
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.json(err));
+    },
+
     // remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
